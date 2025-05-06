2fdf54052a17f8cda6ec8f7e1acf67ca420623ea
Elly <167252820+GhoostSoul@users.noreply.github.com>
2025-05-06 16:16:06 -0300

    Add files via upload
    criação de conta e configuração do EmailJS para funcionalidade do formulário de contato, mais modificações de personalização em breve,

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+
 const projetos = document.querySelectorAll('.projeto');
 projetos.forEach(projeto => {
-    projeto.addEventListener('mouseenter', () => {
-        projeto.style.backgroundColor = '#f0f0f0';
-    });
-    projeto.addEventListener('mouseleave', () => {
-        projeto.style.backgroundColor = '';
-    });
+  projeto.addEventListener('mouseenter', () => {
+    projeto.style.backgroundColor = '#f0f0f0';
+  });
+  projeto.addEventListener('mouseleave', () => {
+    projeto.style.backgroundColor = '';
+  });
 });
-document.getElementById('form-contato').addEventListener('submit', function(event) {
-    const nome = document.getElementById('nome').value;
-    const mensagem = document.getElementById('mensagem').value;
 
-    if (!nome || !mensagem) {
-        alert('Por favor, preencha todos os campos!');
-        event.preventDefault();
-    } else {
-        alert('Mensagem enviada com sucesso!');
-    }
+
+(function () {
+  emailjs.init("Szrf4fiPtJWHnS_Ihw"); 
+})();
+
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  emailjs.sendForm('service_5j7nxfy', 'template_4x5pjie', this)
+    .then(function () {
+      alert('Mensagem enviada com sucesso!');
+      document.getElementById('contact-form').reset(); 
+    }, function (error) {
+      console.log('Erro:', error);
+      alert('Ocorreu um erro ao enviar. Tente novamente.');
+    });
 });
+
