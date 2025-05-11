8a82dd91a820bec07782c09067469e4b0e520940
Elly <167252820+GhoostSoul@users.noreply.github.com>
2025-05-11 13:50:01 -0300

    manutenção da key
    correção da key associada ao EmailJS

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function () {
-  emailjs.init("Szrf4fiPtJWHnS_Ihw"); 
+  emailjs.init("zrf4fiPtJWHnS_Ihw"); 
 })();
 
 
 document.getElementById('contact-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  emailjs.sendForm('service_5j7nxfy', 'template_4x5pjie', this)
+  emailjs.sendForm('service_l0g6bx5', 'template_4x5pjie', this)
     .then(function () {
       alert('Mensagem enviada com sucesso!');
       document.getElementById('contact-form').reset(); 
